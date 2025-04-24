80c80d8a428f3cc0a009ae6df74ecc8c77db8a40
gladhee <gladhee@icloud.com>
2025-04-24 15:28:06 +0900

    feat(articleHandler): 글 작성 폼 보여주는 엔드포인트 구현

---
+++ src/main/java/handler/ArticleHandler.java
@@ -0,0 +1,19 @@
+package handler;
+
+import model.User;
+import webserver.annotation.RequestMapping;
+import webserver.http.common.HttpSession;
+
+public class ArticleHandler {
+
+    @RequestMapping(method = "GET", path = "/articles")
+    public String showWritePage(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "index";
+        }
+
+        return "article/index";
+    }
+
+}
