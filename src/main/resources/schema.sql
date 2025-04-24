91602353ad57399489230908cf03401431369fb2
gladhee <gladhee@icloud.com>
2025-04-24 17:19:00 +0900

    fix(schema): 스네이크 케이스로 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,17 @@
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
     id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    userId   VARCHAR(50),
+    user_id   VARCHAR(50),
     name      VARCHAR(100) NOT NULL,
     password  VARCHAR(100) NOT NULL,
     email     VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS articles (
+CREATE TABLE articles (
     id        BIGINT AUTO_INCREMENT PRIMARY KEY,
     content   VARCHAR(1000) NOT NULL,
-    userId VARCHAR(50) NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(id)
+    user_id   BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
