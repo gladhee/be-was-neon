8e37a57f791aa8d3ac76ada11b5fef5166105c74
gladhee <gladhee@icloud.com>
2025-04-24 16:05:45 +0900

    feat(schema): 테이블 스키마 작성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    userId   VARCHAR(50),
+    name      VARCHAR(100) NOT NULL,
+    password  VARCHAR(100) NOT NULL,
+    email     VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content   VARCHAR(1000) NOT NULL,
+    userId VARCHAR(50) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
