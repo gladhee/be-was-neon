26cfd663363a848e5cadfbd9ac66545475af49b0
gladhee <gladhee@icloud.com>
2025-04-24 22:05:18 +0900

    feat(ArticleHandler): 글쓰기 엔드포인트 작성

--- src/main/java/handler/Article/ArticleHandler.java
+++ src/main/java/handler/Article/ArticleHandler.java
@@ -6,6 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.annotation.RequestMapping;
 import webserver.http.common.HttpSession;
+import webserver.http.request.HttpRequest;
+import webserver.resolver.ResolveResponse;
+import webserver.util.QueryStringParser;
+
+import java.util.Map;
 
 public class ArticleHandler {
 
@@ -29,6 +34,23 @@ public class ArticleHandler {
 
         logger.debug("user is {}", user);
         return "article/index";
+    }
+
+    @RequestMapping(method = "POST", path = "/articles")
+    public ResolveResponse<String> create(HttpRequest request, HttpSession session) {
+        logger.debug("create");
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            logger.debug("user is null");
+            return ResolveResponse.redirect("login");
+        }
+
+        String body = request.getBody();
+        Map<String, String> queryString = QueryStringParser.parse(body);
+        String content = queryString.get("content");
+        Article article = new Article(content, user.getName());
+        articleDao.save(article);
+        return ResolveResponse.redirect("/");
     }
 
 }
